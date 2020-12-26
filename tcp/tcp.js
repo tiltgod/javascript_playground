91da1a92bd7f4a5560bdeac2499655547c3a2bdf
tiltgod <59070084@it.kmitl.ac.th>
2020-12-26 15:22:22 +0700

    Adding comment in tcp.js

--- tcp/tcp.js
+++ tcp/tcp.js
@@ -1,7 +1,7 @@
 const net = require("net")
 
 const server = net.createServer(socket => {
-    socket.write("Hello.")
+    socket.write("Hello.") //say to client
     socket.on("data", data => {
         console.log(data.toString())
     })
